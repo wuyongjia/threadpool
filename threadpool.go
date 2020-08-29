60c1b5367da38012a2fd1a327da8ba3307b0e178
Rinky <61883659@qq.com>
2020-08-29 15:34:53 +0800

    add pool sequence

--- threadpool.go
+++ threadpool.go
@@ -2,22 +2,51 @@ package threadpool
 
 import (
 	"errors"
+	"sync"
 )
 
 type Pool struct {
-	Args    chan interface{}
-	Threads int
-	Func    func(interface{})
+	Args        chan interface{}
+	Threads     int
+	QueueLength int
+	Func        func(interface{})
 }
 
-func NewWithFunc(numberOfThreads int, maxQueueLength int, poolFunc func(interface{})) *Pool {
-	if maxQueueLength < numberOfThreads {
+type PoolSequence struct {
+	ArgsList    []chan interface{}
+	Id          int
+	Threads     int
+	QueueLength int
+	Func        func(interface{})
+	Lock        *sync.RWMutex
+}
+
+func NewWithFunc(threads int, queueLength int, poolFunc func(interface{})) *Pool {
+	if queueLength < threads {
 		panic(errors.New("the maximum queue length must be greater than or equal to the number of threads"))
 	}
 	var pool = &Pool{
-		Args:    make(chan interface{}, maxQueueLength),
-		Threads: numberOfThreads,
-		Func:    poolFunc,
+		Args:        make(chan interface{}, queueLength),
+		Threads:     threads,
+		QueueLength: queueLength,
+		Func:        poolFunc,
+	}
+	pool.start()
+	return pool
+}
+
+func NewSequenceWithFunc(threads int, queueLength int, poolFunc func(interface{})) *PoolSequence {
+	var pool = &PoolSequence{
+		ArgsList:    make([]chan interface{}, threads),
+		Id:          0,
+		Threads:     threads,
+		QueueLength: queueLength,
+		Func:        poolFunc,
+		Lock:        &sync.RWMutex{},
+	}
+	var i int
+	for i = 0; i < threads; i++ {
+		pool.ArgsList[i] = make(chan interface{}, queueLength)
 	}
 	pool.start()
 	return pool
@@ -25,10 +54,10 @@ func NewWithFunc(numberOfThreads int, maxQueueLength int, poolFunc func(interfac
 
 func (p *Pool) start() {
 	var i int
-	var ok bool
-	var args interface{}
 	for i = 0; i < p.Threads; i++ {
 		go func() {
+			var ok bool
+			var args interface{}
 			for {
 				args, ok = <-p.Args
 				if ok {
@@ -45,4 +74,47 @@ func (p *Pool) Invoke(args interface{}) {
 
 func (p *Pool) Close() {
 	close(p.Args)
+}
+
+func (p *PoolSequence) start() {
+	var i int
+	for i = 0; i < p.Threads; i++ {
+		var ch = p.ArgsList[i]
+		go func() {
+			var ok bool
+			var args interface{}
+			for {
+				args, ok = <-ch
+				if ok {
+					p.Func(args)
+				}
+			}
+		}()
+	}
+}
+
+func (p *PoolSequence) GetSequenceId() int {
+	p.Lock.Lock()
+	defer p.Lock.Unlock()
+	var id = p.Id
+	p.Id++
+	if p.Id >= p.Threads {
+		p.Id = 0
+	}
+	return id
+}
+
+func (p *PoolSequence) Invoke(id int, args interface{}) {
+	if id >= 0 {
+		p.ArgsList[id] <- args
+	} else {
+		p.ArgsList[p.GetSequenceId()] <- args
+	}
+}
+
+func (p *PoolSequence) Close() {
+	var c chan interface{}
+	for _, c = range p.ArgsList {
+		close(c)
+	}
 }
